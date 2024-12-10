9f929acdf395caf075cef4d4cfd23592f09c7280
pipemaldonado70 <felipe.maldonado41@gmail.com>
2024-12-09 21:29:53 -0500

    se agrega extraccion de los reportes gcp y oci

--- dags/dag_prueba_tecnica.py
+++ dags/dag_prueba_tecnica.py
@@ -17,7 +17,8 @@ cluster_name = "cluster-d649"  # Nombre del clúster de Dataproc
 project_id = "etlprueba"  # ID de tu proyecto en Google Cloud
 region = "us-central1"  # Región de tu clúster (ajusta según tu región)
 bucket = f"almacenamiento_etl_prueba/archivos_reporte/{fecha_actual}"
-bucket_aws = "almacenamiento_aws"
+bucket_aws = "pipe-maldonado-0"
+bucket_oci = "almacenamiento_oci"
 
 # Define el DAG
 default_args = {
@@ -37,9 +38,20 @@ with models.DAG(
 
     descargar_aws_reporte = SSHOperator(
         task_id='descarga_reporte_aws',
-        ssh_conn_id='your_ssh_connection_id',  # Configura tu conexión SSH en Airflow
+        ssh_conn_id='your_ssh_connection_id',
         command=f'aws s3 cp s3://{bucket_aws}/aws_facturacion_{fecha_actual}.csv gs://{bucket}/archivos_reporte/{fecha_actual}/'
     )
 
+    descargar_oci_reporte = SSHOperator(
+        task_id='descarga_reporte_oci',
+        ssh_conn_id='your_ssh_connection_id',
+        command=f'oci os object get --{bucket_oci} {bucket} --name oci_fact_{fecha_actual}.csv --file /tmp/oci_reporte{fecha_actual}.txt'
+    )
 
-descargar_aws_reporte
+    descargar_gcp_reporte = SSHOperator(
+        task_id='descarga_reporte_gcp',
+        ssh_conn_id='your_ssh_connection_id',  # Configura tu conexión SSH en Airflow
+        command='gcloud storage cp gs://your-bucket/gcp_costos_mensuales_YYYYMM.csv gs://your-bucket/archivos_reporte/20241209/',
+    )
+
+[descargar_aws_reporte >> descargar_oci_reporte >> descargar_gcp_reporte]
