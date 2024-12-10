2de01b7b486043afc43e99a1f28c915b1e465709
pipemaldonado70 <felipe.maldonado41@gmail.com>
2024-12-09 23:00:43 -0500

    se crea diccionario para crear campo categoria, se hace la union entre dfs

--- jobs/job_etl_prueba_tecnica.py
+++ jobs/job_etl_prueba_tecnica.py
@@ -25,38 +25,86 @@ if __name__ == "__main__":
         FECHA_ACTUAL = datetime.datetime.now().strftime("%Y%m%d")
         GCS_BUCKET = "gs://almacenamiento_etl_prueba/archivos_reporte/"
         RUTA_ARCHIVOS = GCS_BUCKET + FECHA_ACTUAL + "/"
+        SERVICIOS_CLOUD = {
+            "AWS": {
+                "Elastic Compute Cloud": {"categoria": "Cómputo"},
+                "Virtual Private Cloud": {"categoria": "Red"},
+                "CloudWatch": {"categoria": "Monitoreo"},
+                "Glue": {"categoria": "Procesamiento de Datos"},
+                "Simple Storage Service": {"categoria": "Almacenamiento"},
+            },
+            "OCI": {
+                "Oracle Cloud Compute": {"categoria": "Cómputo"},
+                "Oracle Cloud Virtual Cloud Network (VCN)": {"categoria": "Red"},
+                "Oracle Cloud Monitoring and Logging": {"categoria": "Monitoreo"},
+                "Oracle Cloud Data Integration": {"categoria": "Procesamiento de Datos"},
+                "Oracle Cloud Object Storage": {"categoria": "Almacenamiento"},
+            },
+            "GCP": {
+                "Google Compute Engine (GCE)": {"categoria": "Cómputo"},
+                "Google Virtual Private Cloud": {"categoria": "Red"},
+                "Google Cloud Operations Suite": {"categoria": "Monitoreo"},
+                "Google Cloud DataProc": {"categoria": "Procesamiento de Datos"},
+                "Google Cloud Storage (GCS)": {"categoria": "Almacenamiento"},
+            }
+        }
 
         # leer archivos
         aws_data = spark.read.option("header", "true").csv(
-            f"{RUTA_ARCHIVOS}/aws_facturacion_{fecha_actual}.csv"
+            f"{RUTA_ARCHIVOS}/aws_facturacion_{FECHA_ACTUAL}.csv"
         )
         oci_data = spark.read.option("header", "true").csv(
-            f"{ruta_archivos}/oci_fact_{fecha_actual}.csv"
+            f"{ruta_archivos}/oci_fact_{FECHA_ACTUAL}.csv"
         )
         gcp_data = spark.read.option("header", "true").csv(
-            f"{ruta_archivos}/gcp_costos_diarios_{fecha_actual}.csv"
+            f"{ruta_archivos}/gcp_costos_diarios_{FECHA_ACTUAL}.csv"
         )
 
+        # seleccion de columnas
         aws_data = aws_data.select(
             col("account_id").alias("id_cuenta"),
             col("fecha").alias("fecha_reporte"),
             "servicio",
             col("importe_en_usd").alias("total"),
-        )
+        ).withColumn("total_ARS",lit(0)).\
+            withColumn("nube", lit("AWS"))
 
         oci_data = oci_data.select(
             col("account_id").alias("id_cuenta"),
             col("fecha").alias("fecha_reporte"),
             "servicio",
             "total",
-        )
+        ).withColumn("total_ARS",col("total")).\
+            withColumn("nube", lit("Oracle"))
 
         gcp_data = gcp_data.select(
             col("account_id").alias("id_cuenta"),
             col("fecha").alias("fecha_reporte"),
             "servicio",
             col("importe_en_usd").alias("total"),
-        )
+        ).withColumn("total_ARS",lit(0)).\
+            withColumn("nube", lit("GCP"))
+
+        # transformar 
+        oci_data = oci_data.withColumn("total",col("total") / 500)
+
+        # union dfs
+        df = aws_data.union(oci_data).union(gcp_data)
+        
+        # creacion campo categoria de acuerdo al diccionario
+        servicio_categoria = []
+        for cloud, services in SERVICIOS_CLOUD.items():
+            for service, details in services.items():
+                servicio_categoria.append((service, details["categoria"]))
+
+        # creacion dataframe con las categorías
+        categoria_df = spark.createDataFrame(servicio_categoria, ["servicio", "categoria"])
+
+        # unir el dataframe de facturación con el dataframe de categorías
+        df = df.join(categoria_df, on="servicio", how="left")
+
+        # Si no se encuentra la categoría, asignar "NA"
+        df = df.withColumn("categoria", when(col("categoria").isNull(), lit("NA")).otherwise(col("categoria")))
 
     except Exception as e:
         raise f"Error inesperado {e}"
