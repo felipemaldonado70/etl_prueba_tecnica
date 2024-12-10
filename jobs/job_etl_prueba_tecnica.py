d48eb4540e94c450004e6d61526bb85352d9a004
pipemaldonado70 <felipe.maldonado41@gmail.com>
2024-12-09 22:12:14 -0500

    se inicializa documentacion modulo (google)

---
+++ jobs/job_etl_prueba_tecnica.py
@@ -0,0 +1,8 @@
+# -- coding: utf-8 --
+""" Este modulo contiene la logica que lee los reportes de facturacion y los transforma para guardarlos
+    en bigquery para su correspondiente analisis
+"""
+ 
+_author_ = "Luis Felipe Maldonado"
+_maintainer_ = "Equipo Desarrollo"
+_docformat_ = "Google"
